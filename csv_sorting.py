dc88d7b13990952868a468099a3ff00bd2c8b435
youuijin <uijin0508@gmail.com>
2024-08-20 17:24:24 +0900

    add: gitignore, csv sorting

--- csv_sorting.py
+++ csv_sorting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 file_names = ['resnet18']
-dir_names = ['csvs/env1', 'csvs/env2', 'csvs/env3']
+dir_names = ['csvs/env1', 'csvs/env2', 'csvs/env3/cifar10', 'csvs/env4/cifar10', 'csvs/env4/cifar100']
 # models = ['resnet18', 'wrn_34_10']
 
 for d in dir_names:
@@ -21,3 +21,11 @@ df.sort_values(by = ['test_eps', 'env', 'log_name'], ascending=[True, True, True
 # df.sort_index() # 되돌리기
 
 df.to_csv(f'test.csv', index=False)
+
+df = pd.read_csv(f'test_black.csv')
+# sorted_df = df.sort_values(by=['column1', 'column2'], inplace)
+df.sort_values(by = ['test_eps', 'env', 'log_name'], ascending=[True, True, True], inplace=True)
+# df['model'] = df['model'].str.replace('/', '_')
+# df.sort_index() # 되돌리기
+
+df.to_csv(f'test_black.csv', index=False)
